aec900d7445ec80b9880e37cafcc51f22b7f73da
Nabil Dabouz <nbl.dabouz@gmail.com>
2020-05-29 11:11:24 -0400

    Add titles to the experiences + Updating the descriptions

--- portfolio/src/main/webapp/js/script.js
+++ portfolio/src/main/webapp/js/script.js
@@ -41,23 +41,46 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Update the experience description
 function updateExperienceDescription(company) {
-    // Get the text element by id
+    // Get the text and title elements by id
     var description = document.getElementById('experience-description');
+    var title = document.getElementById('experience-title');
     switch (company) {
         case 'Google':
+            title.innerText = 'Google STEP Intern 2020';
             description.innerText = 'Google STEP Intern 2020';
             break;
         case 'Koc University':
-            description.innerText = 'Koc University Undergraduate Reserach Program 2019';
+            title.innerText = 'Koc University Undergraduate Reserach Program 2019';
+            description.innerText = '-Conducted a research on Rendering and Animating Large Point Clouds in Unity3D as part of developing the digital twin of Koç University’s Campus.\
+                -Effectively used the Shaders and Meshes to render and animate a large number of Points with an interesting Frame Rate (depending on the hardware characteristics).\
+                -Fastly mastered the fundamentals of VR development, C# coding, and Game development with Unity3D.';
             break;
         case 'Polytechnique Montreal':
-            description.innerText = 'Teaching Assistant 2019-2020';
+            title.innerText = 'Teaching Assistant 2019-2020';
+            description.innerText = '- Courses: Object Oriented Programming and Procedural Programming\
+                - Prepared, tested and corrected assignments in C++ for freshman and sophomore students\
+                - Effectively analysing the students’ codes to give critical feedback based on experience\
+                - Provided very detailed explanation to both technical and theoretical concepts in coding';
             break;
         case 'Fusion Jeunesse':
-            description.innerText = 'Robotics Coordinator 2018-2020';
+            title.innerText = 'Robotics Coordinator 2018-2020';
+            description.innerText = 'Youth Fusion is an award-winning charity that contributes to perseverance, employability and civic engagement of youth by implementing\
+                innovative experiential learning projects that create ongoing links between school systems and the community.\
+                As a FIRST Lego Leauge Project Coordinator, my reposibilities are to:\
+                - Mentor primary and/or secondary school students in Lego Robotics\
+                - Prepare the students to participate in FIRST Lego League.\
+                - Organise and implement different activities for students to help them link their studies with the project and its impact on the world.';
             break;
         case 'World Learning':
-            description.innerText = 'STEM Education Intern 2018';
+            title.innerText = 'STEM Education Intern 2018';
+            description.innerText = 'The Algiers STEAM Center strengthens the innovation, critical thinking, and communication skills of the Algerian workforce via direct training of youth,\
+                targeted teacher training, and community education forums and events.\
+                As a STEM Education Intern, my responsibilities were:\
+                - Managing and implementing the Algiers STEAM Center’s summer activities.\
+                - Planning, organising and supervising an intensive robotics training for high school student.\
+                - Selecting, guiding and mentoring the team that participated in the 2018 FIRST Global Challenge.\
+                - Providing support in implementing new STEM regular workshops (Level Up Village - Energy for me).\
+                - Providing guidance and support to the mentors in delivering their workshops.';
             break;
     }
 }
