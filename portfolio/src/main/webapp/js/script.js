85721b8cf28fa57f65f7590fadb213f4f48c0e09
Nabil Dabouz <nbl.dabouz@gmail.com>
2020-06-01 12:20:25 -0400

    Break up the string constantes to make sure the code respects the js style guide

--- portfolio/src/main/webapp/js/script.js
+++ portfolio/src/main/webapp/js/script.js
@@ -14,42 +14,66 @@
 
 // Define the companies' descriptions table
 const abouts = [
-    'Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, a search engine, cloud computing, software, and hardware.',
-    'Koç University is a non-profit private university in Istanbul, Turkey. It started education in temporary buildings in İstinye in 1993, and moved to its current Rumelifeneri campus near Sarıyer in 2000.\
-     Koç University is ranked highest in Turkey according to the 2018 Times Higher Education World University Rankings.',
-    'The Polytechnique Montréal is an engineering school affiliated with the Université de Montréal in Montreal, Quebec, Canada. It ranks first in Canada for the scope of its engineering research.\
-     It is occasionally referred to as Montreal Polytechnic, although in Quebec English its French name is more commonly used.',
-    'Youth Fusion is an award-winning charity that contributes to perseverance, employability and civic engagement of youth by implementing\
-     innovative experiential learning projects that create ongoing links between school systems and the community.',
-    'The Algiers STEAM Center strengthens the innovation, critical thinking, and communication skills of the Algerian workforce via direct training of youth,\
-     targeted teacher training, and community education forums and events.',
+    'Google LLC is an American multinational technology company that\
+    specializes in Internet-related services and products, which include\
+    online advertising technologies, a search engine, cloud computing,\
+    software, and hardware.',
+    'Koç University is a non-profit private university in Istanbul, Turkey.\
+    It started education in temporary buildings in İstinye in 1993, and\
+    moved to its current Rumelifeneri campus near Sarıyer in 2000.\
+     Koç University is ranked highest in Turkey according to the 2018 Times\
+    Higher Education World University Rankings.',
+    'The Polytechnique Montréal is an engineering school affiliated with the\
+    Université de Montréal in Montreal, Quebec, Canada. It ranks first in\
+    Canada for the scope of its engineering research.\
+     It is occasionally referred to as Montreal Polytechnic, although in\
+     Quebec English its French name is more commonly used.',
+    'Youth Fusion is an award-winning charity that contributes to\
+    perseverance, employability and civic engagement of youth by implementing\
+     innovative experiential learning projects that create ongoing links\
+     between school systems and the community.',
+    'The Algiers STEAM Center strengthens the innovation, critical thinking,\
+    and communication skills of the Algerian workforce via direct training of\
+    youth, targeted teacher training, & community education forums & events.',
 ];
 
 // Define my responsabilities during my experiences
 const responsabilities = [
     [],
     [
-        'Conducted a research on Rendering and Animating Large Point Clouds in Unity3D as part of developing the digital twin of Koç University’s Campus',
-        'Effectively used the Shaders and Meshes to render and animate a large number of Points with an interesting Frame Rate (depending on the hardware characteristics)',
-        'Fastly mastered the fundamentals of VR development, C# coding, and Game development with Unity3D',
+        'Conducted a research on Rendering and Animating Large Point Clouds in\
+         Unity3D as part of developing the digital twin of Koç University’s \
+         Campus',
+        'Effectively used the Shaders and Meshes to render and animate a large\
+         number of Points with an interesting Frame Rate (depending on the \
+         hardware characteristics)',
+        'Fastly mastered the fundamentals of VR development, C# coding, and \
+        Game development with Unity3D',
     ],
     [
         'Courses: Object Oriented Programming and Procedural Programming',
-        'Prepared, tested and corrected assignments in C++ for freshman and sophomore students',
-        'Effectively analysed the students’ codes to give critical feedback based on experience',
-        'Provided very detailed explanation to both technical and theoretical concepts in coding',
+        'Prepared, tested and corrected assignments in C++ for freshman and \
+        sophomore students',
+        'Effectively analysed the students’ codes to give critical feedback \
+        based on experience',
+        'Provided very detailed explanation to both technical and theoretical \
+        concepts in coding',
     ],
     [
         'Mentor primary and/or secondary school students in Lego Robotics',
         'Prepare the students to participate in FIRST Lego League',
-        'Organise and implement different activities for students to help them link their studies with the project and its impact on the world.',
+        'Organise and implement different activities for students to help them\
+         link their studies with the project and its impact on the world.',
     ],
     [
-        'Managing and implementing the Algiers STEAM Center’s summer activities',
-        'Planning, organising and supervising an intensive robotics training for high school student',
-        'Selecting, guiding and mentoring the team that participated in the 2018 FIRST Global Challenge',
-        'Providing support in implementing new STEM regular workshops (Level Up Village - Energy for me)',
-        'Providing guidance and support to the mentors in delivering their workshops',
+        'Managing & implementing the Algiers STEAM Center’s summer activities',
+        'Planning, organising and supervising an intensive robotics training \
+        for high school student',
+        'Selecting, guiding and mentoring the team that participated in the \
+        2018 FIRST Global Challenge',
+        'Providing support in implementing new STEM regular workshops (Level \
+        Up Village - Energy for me)',
+        'Guiding & support to the mentors in delivering their workshops',
     ],
 ]
 
@@ -88,23 +112,33 @@ function updateExperienceDescription(company) {
     switch (company) {
         case 'Google':
             title.innerText = 'Google STEP Intern 2020';
-            setExperienceDescription(description, abouts[0], responsabilities[0]);
+            setExperienceDescription(description, 
+                abouts[0], 
+                responsabilities[0]);
             break;
         case 'Koc University':
-            title.innerText = 'Koc University Undergraduate Reserach Program 2019';
-            setExperienceDescription(description, abouts[1], responsabilities[1]);
+            title.innerText = 'Koc University Undergraduate Reserach Program';
+            setExperienceDescription(description, 
+                abouts[1], 
+                responsabilities[1]);
             break;
         case 'Polytechnique Montreal':
             title.innerText = 'Teaching Assistant 2019-2020';
-            setExperienceDescription(description, abouts[2], responsabilities[2]);
+            setExperienceDescription(description, 
+                abouts[2], 
+                responsabilities[2]);
             break;
         case 'Fusion Jeunesse':
             title.innerText = 'Robotics Coordinator 2018-2020'; 
-            setExperienceDescription(description, abouts[3], responsabilities[3]);
+            setExperienceDescription(description, 
+                abouts[3], 
+                responsabilities[3]);
             break;
         case 'World Learning':
             title.innerText = 'STEM Education Intern 2018';
-            setExperienceDescription(description, abouts[4], responsabilities[4]);
+            setExperienceDescription(description, 
+                abouts[4], 
+                responsabilities[4]);
             break;
     }
 }
