2a6af2c844f13024ce081c4c89c5eb1e5132bf07
Nabil Dabouz <ndabouz@google.com>
2020-06-04 17:30:38 +0000

    Add an answer property to the Question

--- portfolio/src/main/java/com/google/sps/servlets/Question.java
+++ portfolio/src/main/java/com/google/sps/servlets/Question.java
@@ -3,11 +3,13 @@ package com.google.sps.servlets;
 public class Question {
     public long id;
     public String message;
+    public String answer;
     public long time;
 
-    public Question(long id, String message, long time) {
+    public Question(long id, String message, String answer, long time) {
         this.id = id;
         this.message = message;
+        this.answer = answer;
         this.time = time;
     }
 }
