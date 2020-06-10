f29ceb3dd3ccf4d795b79a1e1a39cf7424e48a98
Nabil Dabouz <ndabouz@google.com>
2020-06-10 14:11:57 +0000

    Add an upload url servlet that uses the blobstore service

---
+++ portfolio/src/main/java/com/google/sps/servlets/UploadUrlServlet.java
@@ -0,0 +1,26 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+
+@WebServlet("blobstore-upload-url")
+public class UploadUrlServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+        String uploadUrl = blobstoreService.createUploadUrl("/form-handler");
+
+        response.setContentType("text/html");
+        response.getWriter().println(uploadUrl);
+    }    
+}
