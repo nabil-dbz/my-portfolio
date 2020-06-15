e46ba1979e934cc71c94a05d22c426a90557cd56
Nabil Dabouz <ndabouz@google.com>
2020-06-15 21:13:36 +0000

    Add a function to forward the clicked question id to the answer.html page

--- portfolio/src/main/java/com/google/sps/servlets/AddAnswerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/AddAnswerServlet.java
@@ -2,6 +2,8 @@ package com.google.sps.servlets;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +15,8 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 
 /** Servlet that returns some example content. **/
 @WebServlet("/add-answer")
@@ -20,6 +24,22 @@ public class AddAnswerServlet extends HttpServlet {
 
   private static final long serialVersionUID = 1L;
 
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException { 
+    UserService userService = UserServiceFactory.getUserService();
+    if (!userService.isUserLoggedIn()) {
+      String loginUrl = userService.createLoginURL("/login");
+      response.sendRedirect(loginUrl);
+      return;
+    }
+    if (userService.isUserAdmin()) {
+      RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/answer.jsp");
+      rd.include(request, response);
+      return;
+    }
+    response.sendRedirect("/index.html");
+  }
+  
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String answer = request.getParameter("answer");
@@ -36,6 +56,6 @@ public class AddAnswerServlet extends HttpServlet {
       entity.setProperty("answer", answer);
       datastore.put(entity);
     }
-    response.sendRedirect("/answer.html");
+    response.sendRedirect("/index.html");
   }
 }
