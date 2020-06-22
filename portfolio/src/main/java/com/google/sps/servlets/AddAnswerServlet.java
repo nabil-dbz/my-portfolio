e40934c044e37daa15d6d14dba2809a9e8ef41f1
Nabil Dabouz <ndabouz@google.com>
2020-06-22 13:27:44 +0000

    Change the name of the variable rd to avoir abbreviations

--- portfolio/src/main/java/com/google/sps/servlets/AddAnswerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/AddAnswerServlet.java
@@ -33,8 +33,8 @@ public class AddAnswerServlet extends HttpServlet {
       return;
     }
     if (userService.isUserAdmin()) {
-      RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/answer.jsp");
-      rd.include(request, response);
+      RequestDispatcher reauestDispatcher = request.getRequestDispatcher("/WEB-INF/answer.jsp");
+      reauestDispatcher.include(request, response);
       return;
     }
     response.sendRedirect("/index.html");
